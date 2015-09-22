7fb8cdd0faff1209b56ef7d74070a933d3bf5d59
justin almeida <justin.h.almeida@gmail.com>
2015-09-22 12:41:06 -0700

    added basic login/signup function/ homepage

--- index.js
+++ index.js
@@ -1,9 +1,71 @@
 var express = require("express"),
-    app = express();
+bodyParser = require('body-parser'),
+path = require('path'),
+session = require('express-session'),
+db = require('./models')
+    
+    
+ app = express();
+var views = path.join(process.cwd(), "views");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/static", express.static("public"));
+app.use("/vendor", express.static("bower_components"));
+app.use(session({secret: "super secret key", resave: false, saveUninitialized: true}));
+app.use(function(req,res,next){
+	req.login = function (user) {
+		req.session.userId = user._id;
+	};
+	req.currentUser = function (cb) {
+		db.Profile.findOne({_id: req.session.userId}, function (err, user){
+			req.user = user;
+			cb(null, user);
+		})
+	};
+	req.logout = function () {
+		req.session.userId = null;
+		req.user = null;
+	}
+	next();
+})
 
-app.get("/", function (req, res) {
-	res.send("Send me on a Geoquest!");
-});
+
+
+app.get("/", function (req,res) {
+	res.sendFile(path.join(views, "index.html"))
+})
+
+app.get("/home", function (req,res) {
+	res.sendFile(path.join(views, "home.html"))
+})
+
+
+/*Login /signup route */
+app.post('/', function (req,res){
+	console.log(req.body)
+	if(req.body.login === "true") {
+		db.User.authenticate(req.body.username, req.body.password, function (err, user){
+			if(err){
+				console.log(err);
+				res.redirect("/")}
+				else{
+					console.log("logged in");
+					req.login(user)
+					console.log(req.session.userId)
+					res.redirect("/home");}
+				})
+	}
+	else{
+		db.User.createSecure(req.body.username, req.body.password, req.body.homeCity, function(err, user){
+			if(err){return console.log(err);}
+			else{
+				console.log(user);
+				req.login(user);
+				res.redirect("/home");
+			}
+		})
+	}
+	
+})
 
 app.listen(3000, function () {
 	console.log("Geoquest is twerking");
